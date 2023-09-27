a7445cc9b03252aa459aab27cea71f07e7e2d7ba
Verity Scheel <monoidmusician@gmail.com>
2023-09-27 15:36:05 -0500

    Test that backend-optimizer preserves arithmetic expressions

---
+++ test-snapshots/src/snapshots-input/Snapshot.BigInt.purs
@@ -0,0 +1,7 @@
+-- @expected 9007199254740991
+module Snapshot.BigInt where
+
+import Data.Semiring ((+), (*))
+
+result :: Int
+result = 90071992 * 100000000 + 54740991
---
+++ test-snapshots/src/snapshots-output/Snapshot.BigInt.erl
@@ -0,0 +1,5 @@
+-module(snapshot_bigInt@ps).
+-export([result/0]).
+-compile(no_auto_import).
+result() ->
+  ((90071992 * 100000000) + 54740991).
